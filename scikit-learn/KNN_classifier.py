637f41b80443d1699393e916e6898f719ac78d2a
Matvei Aleksandrovich <74256897+MatveiAleksandrovich@users.noreply.github.com>
2021-07-27 18:35:21 +0300

    Update KNN_classifier.py

--- scikit-learn/KNN_classifier.py
+++ scikit-learn/KNN_classifier.py
@@ -1,8 +1,3 @@
-# This is my KNeighborsClassifier creating description
-
-# If you find any mistake or smth i can improve there,
-# please contact me.
-
 import numpy as np
 import pandas as pd
 from sklearn import neighbors, metrics, svm
